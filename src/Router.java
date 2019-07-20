464f15e1856064e3b462b7c983049d36fe88e6f3
JamesC <jchanthamontry@gmali.com>
2019-07-19 19:16:58 -0500

    Initial Code

---
+++ src/Router.java
@@ -0,0 +1,38 @@
+import java.util.LinkedList;
+
+/*
+ * This runnable routes packets as they traverse the network.
+ */
+public class Router implements Runnable {
+    private LinkedList<Packet> list = new LinkedList<Packet>();
+    private int routes[];
+    private Router routers[];
+    private int routerNum;
+    private boolean end = false;
+    Router(int rts[], Router rtrs[], int num) {
+        routes = rts;
+        routers = rtrs;
+        routerNum = num;
+    }
+    /*
+     * Add a packet to this router.  Add some details on how this works.
+     */
+    public void addWork(Packet p) {
+
+    }
+    /*
+     * End the thread, once no more packets are outstanding.
+     */
+    public synchronized void end() {
+
+    }
+
+    public synchronized void networkEmpty() {
+    }
+
+    /*
+     * Process packets.  Add some details on how this works.
+     */
+    public void run() {
+    }
+}
